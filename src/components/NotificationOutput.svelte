61586471b6d6b5b8e796c5e88bbcd8a81085ebb8
Enrico Beyer <enrico.beyer@synaigy.com>
2023-02-19 20:25:33 +0100

    :bug: Add predefined values

--- src/components/NotificationOutput.svelte
+++ src/components/NotificationOutput.svelte
@@ -6,7 +6,7 @@
 
 {#if $notifications}
 	<div class="absolute right-3 top-0 z-[9001]">
-		{#each $notifications as { title, subtitle, caption, kind }}
+		{#each $notifications as { title, subtitle = '', caption = new Date().toLocaleString() , kind = 'info' }}
 			<div transition:fly={{ x: 500 }} class="w-[16rem] md:w-[24rem]">
 				<ToastNotification {kind} {title} {subtitle} {caption} fullWidth />
 			</div>
